5185ff771a9021dc06e977923542cfaa0bb81836
Iro-sources <mohamediro98@gmail.com>
2025-07-05 20:54:46 +0200

    initial commit

---
+++ Day1/main.py
@@ -0,0 +1,26 @@
+print("New lines can be created with a \ and the letter n \n like this")
+
+print("A tab can be created with a \ and the letter t \t like this")
+
+#Switching variables
+glass1 = "milk"
+glass2 = "juice"
+#Only 3 lines are allowed to switch milk and juice
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+
+print(glass1 + "  " + glass2)
+
+#User input
+name = input("What is your name? : \n")
+print("Hello " + " " + name + "!")
+print(f"Hello {name}!")
+
+#Band Name
+print("#####################################")
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+
+print(f"Your band name could be {city} {pet}")
---
+++ Day2/main.py
@@ -0,0 +1,39 @@
+import pyfiglet
+
+#Python Primitive Data Types
+
+#Integeres
+num1 = 9
+num2 = 7
+result = num1 + num2
+print(result)
+
+#Python uses ** to represent exponents
+print(num1 ** num2)
+
+#Python supports the order of operations.
+print(num1 + 2 * num2)
+
+#Floats
+#Pyhton calls any number with a decimal point a **floa**
+a = 0.23
+b = 4.1
+print(a * b)
+
+#Integers and Floats
+#When you divide any two numbers, even if they are integers that result in a whole number,
+#You will always get a float result.
+print(8/2)
+
+#If you mix an **integer** and a float in any other operation, you'll get a float result.
+print(4 + 5.1)
+print(4.0 * 6 )
+
+#Use underscores when you are writing a long number
+decay_time = 45_000_000_000
+
+#BMI Calculator
+ascii_banner = pyfiglet.figlet_format(" BMI CALCULATOR ")
+print(ascii_banner)
+
+
