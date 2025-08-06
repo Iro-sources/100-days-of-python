d5dde83b4cb05495b86335a6a5985b649f0b3290
Iro-sources <mohamediro98@gmail.com>
2025-08-06 23:29:57 +0200

    Love calculator

---
+++ Day8/function_with_inputs.py
@@ -0,0 +1,37 @@
+def greet():
+    print("Hello")
+    print("Good morning")
+    print("Good night")
+
+greet()
+
+
+def life_in_weeks(age):
+    result = 90 - age
+    print(result * 52)
+
+
+life_in_weeks(20)
+
+def check_true_letters(name1, name2):
+    combined_name = name1 + name2
+    word1 = "TRUE"
+    word2 = "LOVE"
+
+    true_count = 0
+    for letter in combined_name.upper():
+        if letter in word1:
+            true_count += 1
+
+
+    love_count = 0
+    for letter in combined_name.upper():
+        if letter in word2:
+            love_count += 1
+
+
+    print (str(true_count) + str(love_count))
+
+
+
+check_true_letters("Angela Yu", "Jack Bauer")
